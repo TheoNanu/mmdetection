4ca6056506085700cee14a9879254fc80fca29c3
TheoNanu <theodor.nanu@gmail.com>
2024-01-14 20:55:31 +0200

    36 epochs

--- configs/swin/mask_rcnn_swin-t-p4-w7_fpn_1x_coco.py
+++ configs/swin/mask_rcnn_swin-t-p4-w7_fpn_1x_coco.py
@@ -41,4 +41,4 @@ optimizer = dict(
             'norm': dict(decay_mult=0.)
         }))
 lr_config = dict(warmup_iters=1000, step=[8, 11])
-runner = dict(max_epochs=12)
+runner = dict(max_epochs=36)
